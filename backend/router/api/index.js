3874256e218f0aba8311bcfb6c6b161a63a377fd
josh <j.maxwell.alan@gmail.com>
2021-06-08 00:49:19 -0400

    instantiate user router

--- backend/router/api/index.js
+++ backend/router/api/index.js
@@ -1,8 +1,11 @@
 import sessionRouter from './session';
+import userRouter from './users';
 import { Router } from 'express';
 const router = Router();
 
 router.use('/session', sessionRouter);
+
+router.use('/users', userRouter);
 
 router.get('/csrf/restore/', (req, res) => {
   const token = req.csrfToken();
