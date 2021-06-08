464afb8eb717a5a0cf4fffd42304700b881fe5a6
Joshua <j.maxwell.alan@gmail.com>
2021-06-08 09:12:29 -0400

    adjust requireAuth to throw custom error

--- backend/utils/requireAuth.js
+++ backend/utils/requireAuth.js
@@ -1,7 +1,12 @@
+import { RequestError } from '../RequestError';
+
 export default function (req, _res, next) {
   if (req.user) return next();
 
-  const err = new Error('Unauthorized');
-  err.status = 401;
+  const err = new RequestError(
+    'Unauthorized',
+    'You must be logged in to access this resource',
+    401
+  );
   next(err);
 }
