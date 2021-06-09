387db7d9ced1a42ecc39f75299f65c6a691b0200
Joshua <j.maxwell.alan@gmail.com>
2021-06-08 22:03:41 -0400

    account creation thunks

--- frontend/src/store/accounts.js
+++ frontend/src/store/accounts.js
@@ -3,6 +3,7 @@ import csrfetch from './csrf';
 const ALL = 'accounts/ALL';
 const PERSONAL = 'accounts/PERSONAL';
 const COMMUNAL = 'accounts/COMMUNAL';
+const CURRENT = 'accounts/CURRENT';
 
 const setAll = all => ({
   type: ALL,
@@ -19,6 +20,11 @@ const setCommunals = communals => ({
   communals
 });
 
+export const setCurrent = current => ({
+  type: CURRENT,
+  current
+});
+
 export const GetAllAccounts = () => async dispatch => {
   const { accounts } = await csrfetch.get('/api/me/accounts/');
   dispatch(setAll(accounts));
@@ -32,6 +38,16 @@ export const GetAllPersonals = () => async dispatch => {
 export const GetAllCommunals = () => async dispatch => {
   const { accounts } = await csrfetch.get('/api/me/communals/');
   dispatch(setCommunals(accounts));
+};
+
+export const CreatePersonal = newAccount => async dispatch => {
+  const { account } = await csrfetch.post('/api/accounts/personals/', newAccount);
+  dispatch(setCurrent(account));
+};
+
+export const CreateCommunal = newAccount => async dispatch => {
+  const { account } = await csrfetch.post('/api/accounts/communals/', newAccount);
+  dispatch(setCurrent(account));
 };
 
 export default function reducer (
