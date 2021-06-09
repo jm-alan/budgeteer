66cc4e9d3ad31b295c00334b79cbacf5999e8651
Joshua <j.maxwell.alan@gmail.com>
2021-06-08 21:46:03 -0400

    custom console log for requestError

--- backend/RequestError.ts
+++ backend/RequestError.ts
@@ -1,3 +1,5 @@
+import util from 'util'
+
 export class RequestError extends Error {
   title: string;
   errors: string[];
@@ -13,11 +15,19 @@ export class RequestError extends Error {
 
 export class ExtendedValidationError {
   title: string;
+  message: string;
   errors: string[];
   status: number
 
-  constructor (title: string) {
+  constructor (title: string, message: string) {
     this.title = title;
+    this.message = message;
     this.status = 401;
+  }
+
+  [util.inspect.custom] (_depth, _opts) {
+    let out = '';
+    for (const error of this.errors) out += `\n    ${error}`;
+    return `--------------------------------------------\n${this.title}\n  ${this.message}${out}`
   }
 }
