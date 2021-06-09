662338cf179a7439539248ddabe549613734dacb
Joshua <j.maxwell.alan@gmail.com>
2021-06-08 22:51:47 -0400

    create current account container

---
+++ frontend/src/components/Accounts/CurrentAccount.js
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+
+export default function CurrentAccount () {
+  const current = useSelector(state => state.accounts.current);
+
+  return current && (
+    <div className='container current-account'>
+      <div className='subcontainer current-account-name'>
+        {current.name}
+      </div>
+      {(current.balance && (
+        <div className='subcontainer current-account-balance'>
+          {current.balance}
+        </div>
+      )) ?? null}
+      <div className='subcontainer current-account-transaction-items'>
+        {current.Items.map((item, idx) => (
+          <div
+            key={idx}
+            className={`transaction-item${item.income ? ' income' : ' expense'}`}
+          >
+            <div className='transaction-item-name'>
+              {item.name}
+            </div>
+            <div className='transaction-item-amount'>
+              {item.amount}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
