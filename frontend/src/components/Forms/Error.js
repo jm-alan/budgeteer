ba3da519af528b7236e33f9b12688073bf71e324
Joshua <j.maxwell.alan@gmail.com>
2021-06-08 22:38:21 -0400

    remove unnecessary console log

--- frontend/src/components/Forms/Error.js
+++ frontend/src/components/Forms/Error.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ error }) => error && (
   <div className='error-bounding-box'>
-    {console.log('RENDERING ERROR COMPONENT')}
     <h4>{error.message}</h4>
     {error.errors.map((err, idx) => (
       <div
