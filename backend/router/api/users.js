2f75aea46a7194787361234de988c09b4157ec7a
Joshua <j.maxwell.alan@gmail.com>
2021-06-09 09:04:00 -0400

    create jwt on signup

--- backend/router/api/users.js
+++ backend/router/api/users.js
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 import asyncHanlder from 'express-async-handler';
+
 import restoreOrReject from '../../utils/restoreOrReject';
+import createToken from '../../utils/createToken';
 import { User } from '../../db/models';
+import { environment, jwtConfig } from '../../config';
 
 const router = Router();
 
 router.post('/', asyncHanlder(async (req, res) => {
   const { body } = req;
   const user = await User.SignUp(body);
+  const token = createToken(user.id);
+  const isProduction = environment === 'production';
+  const cookieOptions = {
+    maxAge: jwtConfig.expiresIn * 1000,
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && 'Lax'
+  };
+  res.cookie('token', token, cookieOptions);
   res.json({ user });
 }));
 
