0f5bf5b105acb7e7a0b63e157e52faffc726400b
Joshua <j.maxwell.alan@gmail.com>
2021-06-10 01:35:34 -0400

    create account delete route

--- backend/router/api/accounts.js
+++ backend/router/api/accounts.js
@@ -5,6 +5,20 @@ import restoreOrReject from '../../utils/restoreOrReject';
 
 const router = Router();
 
+router.delete('/personals/:accountId(\\d+)/', restoreOrReject, asyncHandler(async (req, res) => {
+  const { user, params: { accountId } } = req;
+  const account = (await user.getPersonals()).filter(({ id }) => id === accountId)[0];
+  if (!account) return res.json({ success: true });
+  try {
+    await account.destroy();
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    console.error(err.toString());
+    res.json({ success: false });
+  }
+}));
+
 router.post('/personals/', restoreOrReject, asyncHandler(async (req, res) => {
   const { user, body } = req;
   const account = { ...(await user.createPersonal(body)).dataValues, Items: [] };
