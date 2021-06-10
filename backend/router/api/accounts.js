c3629c75b20d20acc11feeb34d26dd6333b24e98
Joshua <j.maxwell.alan@gmail.com>
2021-06-10 01:41:18 -0400

    communal account delete route

--- backend/router/api/accounts.js
+++ backend/router/api/accounts.js
@@ -19,6 +19,20 @@ router.delete('/personals/:accountId(\\d+)/', restoreOrReject, asyncHandler(asyn
   }
 }));
 
+router.delete('/communals/:accountId(\\d+)/', restoreOrReject, asyncHandler(async (req, res) => {
+  const { user, params: { accountId } } = req;
+  const account = (await user.getCommunals()).filter(({ id }) => id === accountId)[0];
+  if (!account) return res.json({ success: true });
+  try {
+    await user.removeCommunal(account);
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    console.error(err.toString());
+    res.json({ success: false });
+  }
+}));
+
 router.post('/personals/', restoreOrReject, asyncHandler(async (req, res) => {
   const { user, body } = req;
   const account = { ...(await user.createPersonal(body)).dataValues, Items: [] };
