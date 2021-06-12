4706e5cfa4067f003a11380eae050b1e40d66cb3
josh <j.maxwell.alan@gmail.com>
2021-06-12 15:42:46 -0400

    enable cancel button

--- frontend/src/components/Forms/index.js
+++ frontend/src/components/Forms/index.js
@@ -1,14 +1,27 @@
+import { useDispatch } from 'react-redux';
+
 import Error from './Error';
+import { HideModal } from '../../store/UX';
 
 import './index.css';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ({ children, onSubmit, error }) => (
-  <>
-    <Error error={error} />
-    <form className='form auth-form' onSubmit={onSubmit}>
-      {children}
-      <button type='button' className='form-input form-button short cancel'>Cancel</button>
-    </form>
-  </>
-);
+export default function AuthForm ({ children, onSubmit, error }) {
+  const dispatch = useDispatch();
+
+  const onCancel = () => dispatch(HideModal());
+  return (
+    <>
+      <Error error={error} />
+      <form className='form auth-form' onSubmit={onSubmit}>
+        {children}
+        <button
+          type='button'
+          className='form-input form-button short cancel'
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+      </form>
+    </>
+  );
+}
