ce17059aec3557824c05c59efc69b0ec32c4d4ec
josh <j.maxwell.alan@gmail.com>
2021-06-12 19:49:48 -0400

    what's one more prototype method between friends

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import appBuilder from './appBuilder';
 import { ports } from './config';
 
+// eslint-disable-next-line no-extend-native
+String.prototype.upperCaseFirst = function () {
+  return `${this[0].toUpperCase()}${this.slice(1)}`;
+};
+
 const apps = {};
 
 for (const port of ports) appBuilder(apps, port);
