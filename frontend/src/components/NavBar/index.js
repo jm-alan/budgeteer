6cfed28f47050e548166d609753077bd210caf41
Joshua <j.maxwell.alan@gmail.com>
2021-06-12 14:08:49 -0400

    implement classes for styled navbar

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import SignUpForm from '../Forms/SignUp';
 import LogInForm from '../Forms/LogIn';
-import { ShowModal } from '../../store/UX';
+import { HideBar, ShowModal, SideBar } from '../../store/UX';
 import { SetCurrentModal } from '../../store/modal';
 import { LogOut } from '../../store/session';
 
@@ -11,6 +11,7 @@ export default function NavBar () {
   const dispatch = useDispatch();
 
   const user = useSelector(state => state.session.user);
+  const sidebar = useSelector(state => state.UX.sidebar);
 
   const popSignup = () => {
     dispatch(SetCurrentModal(SignUpForm));
@@ -24,44 +25,69 @@ export default function NavBar () {
 
   const logOut = () => dispatch(LogOut());
 
+  const hamburgerClick = (e) => {
+    e.stopPropagation();
+    !sidebar && dispatch(SideBar());
+    sidebar && dispatch(HideBar());
+  };
+
   return (
-    <nav
-      className='navbar'
-    >
-      {user
-        ? (
-          <>
-            <Link to='/'>
-              <button>
-                Home
+    <nav className={sidebar ? 'grow' : 'shrink'}>
+      <button
+        onClick={hamburgerClick}
+        className={`navbar-display${
+          sidebar ? ' close' : ' open'
+        }`}
+      >
+        <div className='navbar-hamburger top' />
+        <div className='navbar-hamburger middle' />
+        <div className='navbar-hamburger bottom' />
+      </button>
+      <div className='nav-sliding-container'>
+        {user
+          ? (
+            <>
+              <div className='highlighter-wrapper'>
+                <Link to='/'>
+                  <button>
+                    Home
+                  </button>
+                </Link>
+                <div className='highlighter' />
+              </div>
+              <div className='highlighter-wrapper'>
+                <Link to='/accounts/'>
+                  <button>
+                    My Accounts
+                  </button>
+                </Link>
+                <div className='highlighter' />
+              </div>
+              <div className='highlighter-wrapper'>
+                <button onClick={logOut}>
+                  Log Out
+                </button>
+                <div className='highlighter' />
+              </div>
+            </>
+            )
+          : (
+            <>
+              <button
+                className='navigation signup'
+                onClick={popSignup}
+              >
+                Sign Up
               </button>
-            </Link>
-            <Link to='/accounts/'>
-              <button>
-                My Accounts
+              <button
+                className='navigation login'
+                onClick={popLogin}
+              >
+                Log In
               </button>
-            </Link>
-            <button onClick={logOut}>
-              Log Out
-            </button>
-          </>
-          )
-        : (
-          <>
-            <button
-              className='navigation signup'
-              onClick={popSignup}
-            >
-              Sign Up
-            </button>
-            <button
-              className='navigation login'
-              onClick={popLogin}
-            >
-              Log In
-            </button>
-          </>
-          )}
+            </>
+            )}
+      </div>
     </nav>
   );
 }
