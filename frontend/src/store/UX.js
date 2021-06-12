5369287b5db3c00245183a289c68b4ea92a1a035
Joshua <j.maxwell.alan@gmail.com>
2021-06-12 14:09:01 -0400

    create sidebar management actions

--- frontend/src/store/UX.js
+++ frontend/src/store/UX.js
@@ -1,13 +1,27 @@
 const SHOWMODAL = 'UX/SHOWMODAL';
-
 const HIDEMODAL = 'UX/HIDEMODAL';
+const SIDEBAR = 'UX/SIDEBAR';
+const HIDEBAR = 'UX/HIDEBAR';
 
-export const ShowModal = () => ({ type: SHOWMODAL });
+export const ShowModal = () => ({
+  type: SHOWMODAL
+});
 
-export const HideModal = () => ({ type: HIDEMODAL });
+export const HideModal = () => ({
+  type: HIDEMODAL
+});
+
+export const SideBar = () => ({
+  type: SIDEBAR
+});
+
+export const HideBar = () => ({
+  type: HIDEBAR
+});
 
 export default function reducer (
-  state = { modal: false },
+  // eslint-disable-next-line default-param-last
+  state = { modal: false, sidebar: false },
   { type }
 ) {
   switch (type) {
@@ -15,6 +29,10 @@ export default function reducer (
       return { ...state, modal: true };
     case HIDEMODAL:
       return { ...state, modal: false };
+    case SIDEBAR:
+      return { ...state, sidebar: true };
+    case HIDEBAR:
+      return { ...state, sidebar: false };
     default:
       return state;
   }
