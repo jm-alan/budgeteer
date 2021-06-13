c022e526307b6b029aab555d53792d7c72a83908
josh <j.maxwell.alan@gmail.com>
2021-06-13 15:59:10 -0400

    copy truncateUntil from frontend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ String.prototype.upperCaseFirst = function () {
   return `${this[0].toUpperCase()}${this.slice(1)}`;
 };
 
+// eslint-disable-next-line no-extend-native
+String.prototype.truncateUntil = function (pattern) {
+  let out = this.toString();
+  while (!out.match(pattern)) out = out.slice(0, out.length - 1);
+  return out;
+};
+
 const apps = {};
 
 for (const port of ports) appBuilder(apps, port);
