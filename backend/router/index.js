4c3105bd90af9d7e2172da866d41d6a56b80982f
josh <j.maxwell.alan@gmail.com>
2021-06-13 15:12:28 -0400

    prep for Heroku deploy

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,8 +1,23 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import { resolve } from 'path';
+
 import apiRouter from './api';
 
 const router = Router();
 
 router.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  const mainIndex = resolve(__dirname, '../../frontend', 'build', 'index.html');
+  router.get('/', (req, res) => {
+    res.sendFile(mainIndex);
+  });
+
+  router.use(express.static(resolve('../webapp/build')));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.sendFile(mainIndex);
+  });
+}
 
 export default router;
