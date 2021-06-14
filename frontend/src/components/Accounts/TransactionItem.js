afb0f09e4f30a3bfbff90ad8b29769d2556c86dc
Joshua <j.maxwell.alan@gmail.com>
2021-06-13 21:18:19 -0400

    add item delete button

--- frontend/src/components/Accounts/TransactionItem.js
+++ frontend/src/components/Accounts/TransactionItem.js
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { DeleteCommunalItem, DeletePersonalItem } from '../../store/accounts/asyncs';
 
 export default function TransactionItem ({ item }) {
   const dispatch = useDispatch();
 
   const current = useSelector(state => state.accounts.current);
 
-  const onDelete = () => {};
+  const onDelete = () => {
+    if (current.balance !== undefined) dispatch(DeletePersonalItem(current.id, item.id));
+    else dispatch(DeleteCommunalItem(current.id, item.id));
+  };
+
   return (
     <div
       className={`entry-item${
@@ -18,6 +23,12 @@ export default function TransactionItem ({ item }) {
       <div className='transaction-item-amount'>
         {item.amount}
       </div>
+      <button
+        className='entry-delete'
+        onClick={onDelete}
+      >
+        -
+      </button>
     </div>
   );
 }
